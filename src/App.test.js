ab77fba81d8ecbd44bca576eee1bf6595163478e
Rob Hooper <95411843+nologyRob@users.noreply.github.com>
2022-11-07 14:10:40 +0000

    Update App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn ract/i);
+  const linkElement = screen.getByText(/learn rct/i);
   expect(linkElement).toBeInTheDocument();
 });
